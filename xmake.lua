69c5995713203599f509cd595a228eb51c2524f8
Hendrik Boeck <hendrikboeck.dev@protonmail.com>
2024-01-29 12:37:19 +0100

    add micro_sockets target

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,12 @@ set_languages("c11")
 
 add_rules("mode.debug", "mode.release")
 
+target("micro_sockets")
+  set_kind("headeronly")
+  add_headerfiles("include/**/*.h")
+  add_rules("utils.install.cmake_importfiles")
+  add_rules("utils.install.pkgconfig_importfiles")
+
 target("examples/tcp_client")
   set_kind("binary")
   add_files("examples/tcp_client.c")
