c9f4801a8fcb9fcd172a7a591f886148e25a757e
Hendrik Böck <hendrikboeck.dev@protonmail.com>
2024-03-21 12:57:06 +0100

    add sockets.h

--- include/micro-sockets/socket.h
+++ include/micro-sockets/socket.h
@@ -17,44 +17,41 @@
 /* with this program.  If not, see <https://www.gnu.org/licenses/>.           */
 /******************************************************************************/
 
-#ifndef __MICRO_SOCKETS_UDP_H
-#define __MICRO_SOCKETS_UDP_H
-
-#include <stdint.h>
-#include <sys/types.h>
-
+#ifndef __MICRO_SOCKETS__SOCKET__H
+#define __MICRO_SOCKETS__SOCKET__H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // clang-format off
-
 #include "micro-sockets/_defs.h"
-
 // clang-format on
 
 #if __MICRO_SOCKETS__IS_WINDOWS
-#include <io.h>
 #include <winsock.h>
-
 #else
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #endif
 
+#include <stdint.h>
+
 #include "ccms/sized_memory.h"
+#include "micro-sockets/sock_addr_any.h"
 
 #if __MICRO_SOCKETS__IS_WINDOWS
 typedef SOCKET SockFd;
 #else
 typedef int32_t SockFd;
-#endif
+#endif  // __MICRO_SOCKETS__IS_WINDOWS
 
-#define UDP_BUFTRUNC 1
+#define RECV_BUF_AUTOTRUNC 1
 
 __MICRO_SOCKETS__INLINE
-ssize_t _udp_recv(SockFd fd, SizedMemory* buf, size_t flags) {
-  int32_t buf_trunc = flags & UDP_BUFTRUNC;
+ssize_t _sock__recv(SockFd fd, SizedMemory* buf, size_t flags) {
+  int32_t buf_trunc = flags & RECV_BUF_AUTOTRUNC;
   size_t buf_size = buf_trunc ? buf->size - 1 : buf->size;
   ssize_t len = recv(fd, buf->ptr, buf_size, 0);
 
@@ -62,10 +59,59 @@ ssize_t _udp_recv(SockFd fd, SizedMemory* buf, size_t flags) {
   return len;
 }
 
+__MICRO_SOCKETS__INLINE
+ssize_t _sock__send(SockFd fd, Box data) {
+  return send(fd, data.ptr, data.size, 0);
+}
+
+__MICRO_SOCKETS__INLINE
+SockFd _sock__new(int32_t ipv, int32_t proto) {
+#if __MICRO_SOCKETS__IS_WINDOWS
+  // Initialize Windows-specific Winsock
+  WORD w_version_requested = MAKEWORD(1, 1);
+  WSADATA wsa_data;
+
+  // If WSAStartup fails, return NULL
+  if (WSAStartup(w_version_requested, &wsa_data) != 0) return NULL;
+#endif
+
+  SockFd sock;
+
+  // Create the socket
+  return socket(ipv, SOCK_STREAM, proto);
+}
+
+__MICRO_SOCKETS__INLINE
+ssize_t _sock__bind(SockFd fd, SockAddrAny* sa, int32_t ipv) {
+  size_t sa_size;
+  struct sockaddr* sa_ref;
+
+  // If the protocol is IPv4
+  if (ipv == AF_INET) {
+    sa_size = sizeof(struct sockaddr_in);
+    sa_ref = _M_cast(struct sockaddr*, _M_addr(sa->inet));
+  }
+  // If the protocol is IPv6
+  else {
+    sa_size = sizeof(struct sockaddr_in6);
+    sa_ref = _M_cast(struct sockaddr*, _M_addr(sa->inet6));
+  }
+
+  // Bind the socket to the address
+  return bind(fd, sa_ref, sa_size);
+}
+
+__MICRO_SOCKETS__INLINE
+int32_t _sock__close(SockFd fd) {
+#if __MICRO_SOCKETS__IS_WINDOWS
+  return closesocket(fd);
+#else
+  return close(fd);
+#endif
+}
+
 #ifdef __cplusplus
 }
 #endif
 
-// TODO: Implement UDP socket functions
-
-#endif  // __MICRO_SOCKETS_UDP_H
+#endif  // __MICRO_SOCKETS__SOCKET__H
--- include/micro-sockets/tcp.h
+++ include/micro-sockets/tcp.h
@@ -56,97 +56,7 @@ extern "C" {
 #include "ccms/box.h"
 #include "ccms/sized_memory.h"
 #include "micro-sockets/sock_addr_any.h"
-
-//
-//
-// ------------------------- TYPES -------------------------
-//
-//
-
-/**
- * @brief Typedef for socket descriptor.
- *
- * On Windows, a SOCKET is used. On UNIX/Linux, an int32_t is used.
- */
-#if __MICRO_SOCKETS__IS_WINDOWS
-/**
- * @brief Typedef for socket descriptor.
- *
- * On Windows, a SOCKET is used
- */
-typedef SOCKET SockFd;
-#else
-/**
- * @brief Typedef for socket descriptor.
- *
- * On UNIX/Linux, an int32_t is used.
- */
-typedef int32_t SockFd;
-#endif
-
-//
-//
-// ------------------------- RAW -------------------------
-//
-//
-
-/**
- * @brief Macro used as a flag for auto truncating a buffer in _tcp_recv
- * function.
- *
- * When this flag is set, the _tcp_recv function will automatically truncate the
- * message with a 0x0 at position received length + 1. Therefore, if this flag
- * is set, the buffer size will be effectively reduced by one.
- */
-#define TCP_BUFTRUNC 1
-
-/**
- * @brief Receives a message from a socket.
- *
- * @param fd The socket descriptor.
- * @param buf The buffer to store the received data.
- * @param flags Flags for receiving message.
- *
- * @return The length of the message received, or -1 on error.
- */
-__MICRO_SOCKETS__INLINE
-ssize_t _tcp_recv(SockFd fd, SizedMemory* buf, size_t flags) {
-  int32_t buf_truncate = flags & TCP_BUFTRUNC;
-  size_t buf_size = buf_truncate ? buf->size - 1 : buf->size;
-  ssize_t len = recv(fd, buf->ptr, buf_size, 0);
-
-  if (len >= 0 && buf_truncate) buf->ptr[len] = 0x0;
-  return len;
-}
-
-/**
- * @brief Sends a message on a socket.
- *
- * @param fd The socket descriptor.
- * @param data The data to send.
- *
- * @return The number of bytes sent, or -1 on error.
- */
-__MICRO_SOCKETS__INLINE
-ssize_t _tcp_send(SockFd fd, Box data) {
-  return send(fd, data.ptr, data.size, 0);
-}
-
-/**
- * @brief Closes a socket.
- *
- * @param fd The socket descriptor.
- *
- * @return 0 on success, or -1 on error.
- */
-__MICRO_SOCKETS__INLINE
-int32_t _tcp_close(SockFd fd) {
-#if __MICRO_SOCKETS__IS_WINDOWS
-  return closesocket(fd);
-#else
-  return close(fd);
-#endif
-}
+#include "micro-sockets/socket.h"
 
 //
 //
@@ -204,7 +114,7 @@ TcpConnection tcp_connection__ctor(SockFd fd, SockAddrAny sa_addr,
  */
 __MICRO_SOCKETS__INLINE
 int32_t tcp_connection__close(TcpConnection* conn) {
-  return _tcp_close(conn->fd);
+  return _sock__close(conn->fd);
 }
 
 /**
@@ -217,7 +127,7 @@ int32_t tcp_connection__close(TcpConnection* conn) {
  */
 __MICRO_SOCKETS__INLINE
 ssize_t tcp_connection__send(TcpConnection* conn, Box data) {
-  return _tcp_send(conn->fd, data);
+  return _sock__send(conn->fd, data);
 }
 
 /**
@@ -232,7 +142,7 @@ ssize_t tcp_connection__send(TcpConnection* conn, Box data) {
 __MICRO_SOCKETS__INLINE
 ssize_t tcp_connection__recv(TcpConnection* conn, SizedMemory* buf,
                              size_t flags) {
-  return _tcp_recv(conn->fd, buf, flags);
+  return _sock__recv(conn->fd, buf, flags);
 }
 
 //
@@ -303,45 +213,22 @@ void tcp_server__free(TcpServer* self) {
 __MICRO_SOCKETS__INLINE
 TcpServer* tcp_server__new(const char* addr, const uint16_t port,
                            const size_t flags) {
-#if __MICRO_SOCKETS__IS_WINDOWS
-  // Initialize Windows-specific Winsock
-  WORD w_version_requested = MAKEWORD(1, 1);
-  WSADATA wsa_data;
-
-  // If WSAStartup fails, return NULL
-  if (WSAStartup(w_version_requested, &wsa_data) != 0) return NULL;
-#endif
-
   // Allocate memory for a new TcpServer
   TcpServer* self = _M_new(TcpServer);
   // Zero out the memory of the new TcpServer
   memset(self, 0, sizeof(TcpServer));
 
   // Determine the protocol based on TCP_SERVER_INET6 flag
-  int32_t proto = (flags & TCP_SERVER_INET6) ? AF_INET6 : AF_INET;
+  int32_t ipv = (flags & TCP_SERVER_INET6) ? AF_INET6 : AF_INET;
 
   // Construct the socket address
-  SockAddrAny sa = sock_addr_any__ctor(proto, addr, port);
-
-  size_t sa_size;
-  struct sockaddr* sa_ref;
-
-  // If the protocol is IPv4
-  if (proto == AF_INET) {
-    sa_size = sizeof(struct sockaddr_in);
-    sa_ref = _M_cast(struct sockaddr*, _M_addr(sa.inet));
-  }
-  // If the protocol is IPv6
-  else {
-    sa_size = sizeof(struct sockaddr_in6);
-    sa_ref = _M_cast(struct sockaddr*, _M_addr(sa.inet6));
-  }
+  SockAddrAny sa = sock_addr_any__ctor(ipv, addr, port);
 
   // Initialize the flags
   self->flags = flags;
 
   // Create the socket
-  self->sock = socket(proto, SOCK_STREAM, IPPROTO_TCP);
+  self->sock = _sock__new(&sa, ipv, IPPROTO_TCP);
 
   // If socket creation failed, free the TcpServer and return NULL
   if (self->sock < 0) {
@@ -349,11 +236,9 @@ TcpServer* tcp_server__new(const char* addr, const uint16_t port,
     return NULL;
   }
 
-  // Bind the socket to the address
-  if (bind(self->sock, sa_ref, sa_size) != 0) {
-    // If binding failed, free the TcpServer and return NULL
+  if (_sock__bind(self->sock, &sa, ipv) != 0) {
     tcp_server__free(self);
-    return NULL;  // check errno();
+    return NULL;
   }
 
   // Return the new TcpServer
@@ -470,7 +355,7 @@ TcpConnection tcp_server__accept(TcpServer* self) {
 __MICRO_SOCKETS__INLINE
 Box tcp_server__recv(TcpServer* self, TcpConnection* conn) {
   // Receive data from the TcpConnection into the server's buffer
-  ssize_t size = tcp_connection__recv(conn, self->buf, TCP_BUFTRUNC);
+  ssize_t size = tcp_connection__recv(conn, self->buf, RECV_BUF_AUTOTRUNC);
 
   // If the receive operation failed, return an empty Box
   if (size < 0) return box__ctor(NULL, 0);
@@ -490,7 +375,7 @@ Box tcp_server__recv(TcpServer* self, TcpConnection* conn) {
  */
 __MICRO_SOCKETS__INLINE
 int32_t tcp_server__shutdown(TcpServer* self) {
-  return _tcp_close(self->sock);
+  return _sock__close(self->sock);
 }
 
 //
@@ -575,15 +460,6 @@ void tcp_client__free(TcpClient* self) {
 __MICRO_SOCKETS__INLINE
 TcpClient* tcp_client__new(const char* addr, const uint16_t port,
                            const size_t flags) {
-#if __MICRO_SOCKETS__IS_WINDOWS
-  // Initialize Windows specific Winsock
-  WORD w_version_requested = MAKEWORD(1, 1);
-  WSADATA wsa_data;
-
-  // If WSAStartup fails, return NULL
-  if (WSAStartup(w_version_requested, &wsa_data) != 0) return NULL;
-#endif
-
   // Allocate memory for a new TcpClient
   TcpClient* self = _M_new(TcpClient);
   // Zero out the memory of the new TcpClient
@@ -602,8 +478,7 @@ TcpClient* tcp_client__new(const char* addr, const uint16_t port,
   // Initialize the server address
   self->_server_addr = sock_addr_any__ctor(self->_proto, addr, port);
 
-  // Initialize the socket
-  self->sock = socket(self->_proto, SOCK_STREAM, IPPROTO_TCP);
+  self->sock = _sock__new(self->_proto, IPPROTO_TCP);
   // If socket creation fails, free the TcpClient and return NULL
   if (self->sock < 0) {
     tcp_client__free(self);
@@ -688,7 +563,7 @@ int32_t tcp_client__connect(TcpClient* self) {
  */
 __MICRO_SOCKETS__INLINE
 int32_t tcp_client__close(TcpClient* self) {
-  return _tcp_close(self->sock);
+  return _sock__close(self->sock);
 }
 
 /**
@@ -720,7 +595,7 @@ TcpConnection tcp_client__as_tcp_connection(const TcpClient* self) {
  */
 __MICRO_SOCKETS__INLINE
 ssize_t tcp_client__send(TcpClient* self, Box data) {
-  return _tcp_send(self->sock, data);
+  return _sock__send(self->sock, data);
 }
 
 /**
@@ -736,7 +611,7 @@ ssize_t tcp_client__send(TcpClient* self, Box data) {
  */
 __MICRO_SOCKETS__INLINE
 Box tcp_client__recv(TcpClient* self) {
-  ssize_t size = _tcp_recv(self->sock, self->buf, TCP_BUFTRUNC);
+  ssize_t size = _sock__recv(self->sock, self->buf, RECV_BUF_AUTOTRUNC);
 
   if (size < 0) return box__ctor(NULL, 0);
 
